1a5f26db6e1581819b7d8fb48f21d50438280686
Davidson <davidson@hotmail.com>
2022-05-05 03:45:24 -0300

    finalizando estilizacao da pagina de detalhes e cadastro

--- resources/views/analise.blade.php
+++ resources/views/analise.blade.php
@@ -12,39 +12,7 @@
         <a class="active" href="/novelas">Novelas</a>
         </div>
 
-        {{--
-        <div class="content">
-            <form action={{ route('criar') }} method="post" enctype="multipart/form-data">
-            @csrf
 
-            Titulo da midia: <input name="titulo" type="text"><br><br>
-            Analise: <br><textarea name="analise" rows="4" cols="50"></textarea><br><br>
-
-            Tipo:
-            <select name="tipo_midias_id">
-
-                @foreach ($tipos as $tipo)
-            
-                    <option value="{{ $tipo->id }}"> {{ $tipo->nome }} </option>
-           
-                 @endforeach
-
-            </select>
-
-            Nota:   <select name="nota">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    </select>
-        <br><br>
-        Capa: <input type="file" name="file" required>
-        <br><br>
-        <button type="submit">Cadastrar</button>
-    
-        </form>
-        </div> --}}
         <div class="content">
             <div class="container">
                 <h1 class="titulo">Cadastro de Mídia</h1>
--- resources/views/detalhe.blade.php
+++ resources/views/detalhe.blade.php
@@ -1,39 +1,21 @@
-<h1>{{ $analise->titulo }}</h1>
+<html>
+<head>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+</head>
+
+<section>
+  <h1>{{$analise->titulo}}</h1>
+  <div class="content2">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, laborum suscipit doloremque, expedita voluptatum maiores corporis modi assumenda esse consectetur error nulla, vero alias.</p>
+  </div>
+  <div class="img2"><img src="{{ asset('storage/images/'.$analise->file_path) }}"></div>
+</section>
 
 
 
-Tipo: {{ $analise->nome }} <br>
-Nota: {{ $analise->nota }}<br>
-Analise: {{ $analise->analise }}
 
 
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-
-<h1>*****Novo Comentário*****</h1><br><br>
-<form action="/salvar_comentario/{{$id}}" method="post">
-    @csrf
-    Seu nome: <input name="nome" type="text"><br><br>
-    Seu email: <input name="email" type="text"><br><br>
-    Comentário: <br><textarea name="mensagem" rows="4" cols="50"></textarea><br><br>
-    <button type="submit">Postar</button>    
-</form>
+<body>
 
 <br>
 <br>
@@ -41,17 +23,26 @@ Analise: {{ $analise->analise }}
 <br>
 <br>
 
-<h1>*****Comentários*****</h1><br>
-
+<div>
+    <h1>Comentários</h1><br><br>
+    <form action="/salvar_comentario/{{$id}}" method="post">
+        @csrf
+        <label class="xxx">Seu nome: </label><input name="nome" type="text"><br><br>
+        <label class="xxx">Seu email: </label><input name="email" type="text"><br><br>
+        <label class="xxx">Comentário: </label><br><textarea name="mensagem" rows="4" cols="50"></textarea><br><br>
+        <button type="submit">Postar</button>    
+    </form>
+</div>
 
 
 @foreach ($comentarios as $comentario)
             
     <h1>{{ $comentario->nome }}</h1>
-    E-mail: {{ $comentario->email }} <br>
-    Mensagem: <br><br> 
-    {{ $comentario->mensagem }} <br><br>
-    ------------------------------------------------
+    Mensagem: <br><br>
+    {{ $comentario->mensagem }} <br>
+    
 
 @endforeach
 
+</body>
+</html>
